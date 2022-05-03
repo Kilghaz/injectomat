3a756b4e13a9ff52fb9941d9c0d4504f65dfbb20
Philipp Jean-Jacques <philipp@hijacksoft.de>
2022-05-03 15:04:39 +0200

    Adjusted the component type to support Vue.js and React components.

--- src/types/component.type.ts
+++ src/types/component.type.ts
@@ -1,1 +1,9 @@
-export type Component = never | unknown;
+import { Component as ReactComponent, FC as ReactFunctionComponent } from 'react';
+import { Component as VueComponent } from 'vue';
+
+export type Component =
+    ReactFunctionComponent |
+    ReactComponent |
+    VueComponent |
+    { name: string } |
+    { displayName: string };
